164b710ce93d7910a2952b02553403c5ad1dcce6
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-04-19 14:57:20 -0300

    Add project files.

---
+++ Divisao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número de divisões que deseja realizar: ");
+            int qtde = int.Parse(Console.ReadLine());
+            double numerador, denominador, resultado;
+            for (int i = 1; i <= qtde; i++)
+            {
+                Console.Write("Digite o númerador e denominador, respectivamente, separados por espaços: ");
+                string[] entrada = Console.ReadLine().Split(' ');
+
+                numerador = double.Parse(entrada[0], CultureInfo.InvariantCulture);
+                denominador = double.Parse(entrada[1], CultureInfo.InvariantCulture);
+
+                if (denominador == 0)
+                    Console.WriteLine("Divisão Impossível");
+                else
+                {
+                    resultado = numerador / denominador;
+                    Console.WriteLine(resultado.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
