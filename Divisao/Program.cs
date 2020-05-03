e3de894407a7c69de232c16dc5108d0531f7e082
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-03 16:10:50 -0300

    Adicionando tratamento de excessões

--- Divisao/Program.cs
+++ Divisao/Program.cs
@@ -7,24 +7,35 @@ namespace Divisao
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite o número de divisões que deseja realizar: ");
-            int qtde = int.Parse(Console.ReadLine());
-            double numerador, denominador, resultado;
-            for (int i = 1; i <= qtde; i++)
+            try
             {
-                Console.Write("Digite o númerador e denominador, respectivamente, separados por espaços: ");
-                string[] entrada = Console.ReadLine().Split(' ');
-
-                numerador = double.Parse(entrada[0], CultureInfo.InvariantCulture);
-                denominador = double.Parse(entrada[1], CultureInfo.InvariantCulture);
-
-                if (denominador == 0)
-                    Console.WriteLine("Divisão Impossível");
-                else
+                Console.Write("Digite o número de divisões que deseja realizar: ");
+                int qtde = int.Parse(Console.ReadLine());
+                double numerador, denominador, resultado;
+                for (int i = 1; i <= qtde; i++)
                 {
-                    resultado = numerador / denominador;
-                    Console.WriteLine(resultado.ToString("F1", CultureInfo.InvariantCulture));
+                    Console.Write("Digite o númerador e denominador, respectivamente, separados por espaços: ");
+                    string[] entrada = Console.ReadLine().Split(' ');
+
+                    numerador = double.Parse(entrada[0], CultureInfo.InvariantCulture);
+                    denominador = double.Parse(entrada[1], CultureInfo.InvariantCulture);
+
+                    if (denominador == 0)
+                        Console.WriteLine("Divisão Impossível");
+                    else
+                    {
+                        resultado = numerador / denominador;
+                        Console.WriteLine(resultado.ToString("F1", CultureInfo.InvariantCulture));
+                    }
                 }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Erro de formatação: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro inesperado: {e.Message}");
             }
         }
     }
